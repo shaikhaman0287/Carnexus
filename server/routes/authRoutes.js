cbd11177e2587ad1a3624818ba0764c67f543c8f
shaikhaman0287 <shaikhaman0287@gmail.com>
2026-04-02 12:20:53 +0530

    Delete server directory

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login, logout, register } = require('../controllers/authController');
-
-router.post('/login', login);
-router.post('/register', register);
-router.post('/logout', logout);
-
-module.exports = router;
--- server/routes/customerRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCustomers } = require('../controllers/customerController');
-
-router.get('/', getCustomers);
-
-module.exports = router;
